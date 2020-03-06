8704775a13f3fef4e8ed83f8e0afbcb6ab0ccc47
Richard Pentecost <richardpentecost1@gmail.com>
2020-03-06 11:54:44 +0000

    refactor testing with helper functions

---
+++ tests/helpers/user-helpers.js
@@ -0,0 +1,12 @@
+exports.signUp = user => new Promise((resolve, reject) => {
+  chai.request(server)
+    .post('/users')
+    .send(user)
+    .end((error, response) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(response);
+      }
+    });
+});
--- tests/users.test.js
+++ tests/users.test.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const User = require('../src/models/user');
+const UserHelpers = require('./helpers/user-helpers');
+const DataFactory = require('./helpers/data-factory');
 
 describe('/users', () => {
   beforeEach(done => {
@@ -10,72 +12,50 @@ describe('/users', () => {
 
   describe('POST /users', () => {
     it('creates a new user in the database', done => {
-      const fakeUser = {
-        firstName: 'Richard',
-        lastName: 'Pentecost',
-        email: 'r@r.com',
-        password: '12345678',
-      };
-      chai.request(server)
-        .post('/users')
-        .send(fakeUser)
-        .end((err, res) => {
-          expect(err).to.equal(null);
+      const data = DataFactory.user();
+      UserHelpers.signUp(data)
+        .then(res => {
           expect(res.status).to.equal(201);
-
           User.findById(res.body._id, (error, user) => {
             expect(error).to.equal(null);
-            expect(user.firstName).to.equal(fakeUser.firstName);
-            expect(user.lastName).to.equal(fakeUser.lastName);
-            expect(user.email).to.equal(fakeUser.email);
-            expect(user.password).not.to.equal(fakeUser.password);
+            expect(user.firstName).to.equal(data.firstName);
+            expect(user.lastName).to.equal(data.lastName);
+            expect(user.email).to.equal(data.email);
+            expect(user.password).not.to.equal(data.password);
             expect(user.password).to.have.length(60);
             expect(res.body).not.to.have.property('password');
           });
           done();
-        });
+        })
+        .catch(error => done(error));
     });
 
     it('validates a users email address', done => {
-      const fakeUser = {
-        firstName: 'Richard',
-        lastName: 'Pentecost',
-        email: 'richard',
-        password: '12345678',
-      };
-      chai.request(server)
-        .post('/users')
-        .send(fakeUser)
-        .end((err, res) => {
-          expect(err).to.equal(null);
+      const data = DataFactory.user({ email: 'richard' });
+      UserHelpers.signUp(data)
+        .then(res => {
           expect(res.status).to.equal(400);
           expect(res.body.errors.email).to.equal('Invalid email address');
           User.countDocuments((error, count) => {
             expect(count).to.equal(0);
           });
           done();
-        });
+        })
+        .catch(error => done(error));
     });
 
     it('validates that a users password is at least 8 characters long', done => {
-      const fakeUser = {
-        firstName: 'Richard',
-        lastName: 'Pentecost',
-        email: 'r@r.com',
-        password: '1234',
-      };
-      chai.request(server)
-        .post('/users')
-        .send(fakeUser)
-        .end((err, res) => {
-          expect(err).to.equal(null);
+      const data = DataFactory.user({ password: '1234' });
+      UserHelpers.signUp(data)
+        .then(res => {
           expect(res.status).to.equal(400);
           expect(res.body.errors.password).to.equal('Password must be at least 8 characters long');
           User.countDocuments((error, count) => {
             expect(count).to.equal(0);
           });
           done();
-        });
+        })
+        .catch(error => done(error));
     });
   });
 });
